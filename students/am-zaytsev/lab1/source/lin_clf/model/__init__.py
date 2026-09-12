fce55ac6c4c82f6cce086b55e24e84dabd468312
Zaytsev Artem <zajtsev.artjom@samlit.net>
2026-09-12 18:24:58 +0300

    Fix init files

--- students/am-zaytsev/lab1/source/lin_clf/model/__init__.py
+++ students/am-zaytsev/lab1/source/lin_clf/model/__init__.py
@@ -1,2 +1,4 @@
-from .LinearClassifier import LinearClassifier
 from .Model import Model
+from .LinearClassifier import LinearClassifier
+
+__all__ = ["Model", "LinearClassifier"]
--- students/am-zaytsev/lab1/source/lin_clf/optimizer/__init__.py
+++ students/am-zaytsev/lab1/source/lin_clf/optimizer/__init__.py
@@ -1,2 +1,4 @@
 from .Optimizer import Optimizer
 from .sgd import SGD
+
+__all__ = ["SGD", "Optimizer"]
