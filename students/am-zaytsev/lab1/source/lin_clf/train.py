a4bc8c553a6581f46312fddc5f98d86a0615652e
Zaytsev Artem <zajtsev.artjom@samlit.net>
2026-09-12 20:33:11 +0300

    Add early stop

--- students/am-zaytsev/lab1/source/lin_clf/train.py
+++ students/am-zaytsev/lab1/source/lin_clf/train.py
@@ -2,6 +2,7 @@ from lin_clf.data.utils import fetch_batch, load_data, preprocess
 from lin_clf.model import LinearClassifier, Model
 from lin_clf.model.WeightInit import init_correlation, init_random
 from lin_clf.optimizer import SGD
+from lin_clf.utils import linear_ab
 from lin_clf.visualization import MetricTracker
 
 
@@ -18,12 +19,13 @@ def _train(
     visual_smooth=0.01,
     train_name="",
     fetch_prob=False,
-    renderer="browser",
+    draw_loss=True,
 ):
     optimizer = SGD(lin_model, momentum_k, h)
 
     tracker = MetricTracker()
     q = None
+    q_list = list()
     prob = None
     for i_epoch in range(epochs):
         if fetch_prob:
@@ -38,6 +40,9 @@ def _train(
         if q is None:
             q = loss
         q = (1 - visual_smooth) * q + visual_smooth * loss
+        q_list.append(q)
+        if len(q_list) > 300:
+            del q_list[0]
         metric_train = float((lin_model.predict(X_train) == y_train).mean())
         metric_test = float((lin_model.predict(X_test) == y_test).mean())
 
@@ -45,8 +50,13 @@ def _train(
         tracker.add_metric("loss", "Q" + train_name, q, i_epoch)
         tracker.add_metric("accuracy", "test" + train_name, metric_test, i_epoch)
         tracker.add_metric("accuracy", "train" + train_name, metric_train, i_epoch)
-
-    # tracker.draw_all(max_points=1000, renderer=renderer)
+        if i_epoch > 300:
+            a, b = linear_ab(q_list)
+            if abs(a) < 1e-5:
+                break
+    print(i_epoch)
+    if draw_loss:
+        tracker.draw_all(max_points=1000)
 
 
 def train_corr(
@@ -72,6 +82,39 @@ def train_corr(
         lin_model,
         epochs,
         h,
+        momentum_k,
+        batch_size,
+        visual_smooth,
+    )
+
+    metric_test = float((lin_model.predict(X_test) == y_test).mean())
+    print(metric_test)
+
+
+def train_speed_sgd(
+    epochs=10_000,
+    h=None,
+    tao=0.01,
+    momentum_k=0.1,
+    batch_size=2**6,
+    visual_smooth=0.01,
+):
+    df_x, df_y = load_data()
+
+    X_train, X_test, y_train, y_test = preprocess(df_x, df_y)
+
+    lin_model = LinearClassifier(tao)
+    n, m = X_train.shape
+
+    init_random(lin_model, n, m)
+    _train(
+        X_train,
+        X_test,
+        y_train,
+        y_test,
+        lin_model,
+        epochs,
+        None,
         momentum_k,
         batch_size,
         visual_smooth,
@@ -108,6 +151,7 @@ def train_multistart(
             momentum_k,
             batch_size,
             visual_smooth,
+            draw_loss=False,
         )
         metric_test = float((lin_model.predict(X_test) == y_test).mean())
         print(metric_test)
@@ -148,7 +192,7 @@ def train_fetch_prob(
 
 
 def main():
-    train_fetch_prob()
+    train_speed_sgd()
 
 
 if __name__ == "__main__":
